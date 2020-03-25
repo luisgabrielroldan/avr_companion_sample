8da429f3587b26ce84fd150aa00755e4d948da06
Gabriel Roldan <luisgabrielroldan@gmail.com>
2020-03-25 17:26:08 -0300

    Create project

---
+++ lib/avr_companion_sample.ex
@@ -0,0 +1,18 @@
+defmodule AVRCompanionSample do
+  @moduledoc """
+  Documentation for AVRCompanionSample.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AVRCompanionSample.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/avr_companion_sample_test.exs
@@ -0,0 +1,8 @@
+defmodule AVRCompanionSampleTest do
+  use ExUnit.Case
+  doctest AVRCompanionSample
+
+  test "greets the world" do
+    assert AVRCompanionSample.hello() == :world
+  end
+end
