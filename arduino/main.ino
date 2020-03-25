06d749f7dbb78c9492561f70ede51700c0e7393f
Gabriel Roldan <luisgabrielroldan@gmail.com>
2020-03-25 18:35:13 -0300

    Adding some interaction between the companions

--- arduino/main.ino
+++ arduino/main.ino
@@ -1,12 +1,73 @@
-/* File: arduino/main.ino */
+#include <Arduino.h>
+
+#define BAUDRATE 9600
+#define SERIAL Serial
+#define LED_PIN LED_BUILTIN
+
+typedef struct {
+  uint8_t code;
+  uint8_t analog_id;
+  uint16_t value;
+} analog_result_t;
 
 void setup() {
-  pinMode(13, OUTPUT);
+  SERIAL.begin(BAUDRATE);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+}
+
+uint8_t read_byte() {
+  while (!SERIAL.available());
+
+  return SERIAL.read();
+}
+
+void set_led() {
+  uint8_t state  = read_byte();
+
+  if (state > 0) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
+
+  SERIAL.write('L');
+  SERIAL.write(state);
+  SERIAL.write(0);
+  SERIAL.write(0);
+}
+
+void read_analog() {
+  analog_result_t result;
+  int analogPins[] = {A0, A1, A2, A3};
+  uint8_t analog_id = read_byte();
+
+  if (analog_id < 4) {
+    result.code = 'A';
+    result.analog_id = analog_id;
+    result.value = analogRead(analogPins[analog_id]);
+
+    SERIAL.write((uint8_t*)&result, sizeof(analog_result_t));
+  }
+}
+
+void cmd() {
+  uint8_t cmd = read_byte();
+
+  switch(cmd) {
+    case 'L':
+      set_led();
+      break;
+    case 'A':
+      read_analog();
+      break;
+    default:
+      break;
+  }
 }
 
 void loop() {
-  digitalWrite(13, HIGH);
-  delay(100);
-  digitalWrite(13, LOW);
-  delay(100);
+  if (SERIAL.available()) {
+    cmd();
+  }
 }
--- lib/avr_companion_sample/arduino.ex
+++ lib/avr_companion_sample/arduino.ex
@@ -1,16 +1,35 @@
 defmodule AVRCompanionSample.Arduino do
   use GenServer
+  alias Circuits.UART
   require Logger
+
   @hex_name "arduino.hex"
   @board :uno
   @port "ttyACM0"
+  @speed 9600
+
   def start_link(args) do
     GenServer.start_link(__MODULE__, args, name: __MODULE__)
   end
 
+  def set_led(led_state) when is_boolean(led_state) do
+    GenServer.cast(__MODULE__, {:set_led, led_state})
+  end
+
   def init(_args) do
     send(self(), :update)
-    {:ok, %{}}
+    {:ok, %{port: nil}}
+  end
+
+  def handle_cast({:set_led, led_state}, state) do
+    value =
+      case led_state do
+        true -> 1
+        false -> 0
+      end
+
+    UART.write(state.port, <<?L, value>>)
+    {:noreply, state}
   end
 
   def handle_info(:update, state) do
@@ -18,12 +37,10 @@ defmodule AVRCompanionSample.Arduino do
     |> AVR.update(@port, @board)
     |> case do
       {:ok, _} ->
-        Logger.info("Arduino updated!")
         send(self(), :ready)
         {:noreply, state}
 
       error ->
-        Logger.error("Arduino update failed!")
         # Delay for not restart too quickly
         :timer.sleep(3000)
         {:stop, error, state}
@@ -31,7 +48,46 @@ defmodule AVRCompanionSample.Arduino do
   end
 
   def handle_info(:ready, state) do
+    {:ok, port} = UART.start_link()
+
+    port_opts = [
+      speed: @speed,
+      framing: Circuits.UART.Framing.FourByte
+    ]
+
+    :ok = UART.open(port, @port, port_opts)
+    state = %{state | port: port}
+    Process.send_after(self(), :read, 1000)
     {:noreply, state}
+  end
+
+  def handle_info(:read, state) do
+    UART.write(state.port, <<?A, 0>>)
+    Process.send_after(self(), :read, 1000)
+    {:noreply, state}
+  end
+
+  def handle_info({:circuits_uart, _, <<?A, analog_id, value::16-little>>}, state) do
+    voltage = Float.round(value * (5.0 / 1023.0), 2)
+    Logger.info("Voltage in A#{analog_id}: #{voltage}v")
+    {:noreply, state}
+  end
+
+  def handle_info({:circuits_uart, _, <<?L, led_state, _::binary>>}, state) do
+    led_state =
+      case led_state do
+        1 -> "ON"
+        0 -> "OFF"
+      end
+
+    Logger.info("Led updated to: #{led_state}")
+    {:noreply, state}
+  end
+
+  def handle_info({:circuits_uart, _, {:error, _} = error}, state) do
+    # Delay for not restart too soon
+    :timer.sleep(3000)
+    {:stop, error, state}
   end
 
   defp hex_name() do
